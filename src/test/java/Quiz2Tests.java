65372dd54ba7f16676f3d4e2a1d46700e1d65922
irinkain <45570725+irinkain@users.noreply.github.com>
2022-10-31 21:26:45 +0400

    Add files via upload

---
+++ src/test/java/Quiz2Tests.java
@@ -0,0 +1,33 @@
+import Data.Constants;
+import Pages.Quiz2Steps;
+import io.restassured.response.Response;
+import org.apache.commons.lang.RandomStringUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Quiz2Tests {
+    Quiz2Steps steps = new Quiz2Steps();
+
+    @Test(priority = 1)
+    public void emptyPassword(){
+        Response response = steps.sendRequest(steps.jsonBody("irina",""));
+        Assert.assertEquals(response.statusCode(),400);
+        Assert.assertEquals(response.jsonPath().getString("message"),Constants.message);
+    }
+
+    @Test(priority = 2)
+    public void emptyUsername(){
+        Response response = steps.sendRequest(steps.jsonBody("","123"));
+        Assert.assertEquals(response.statusCode(),400);
+        Assert.assertEquals(response.jsonPath().getString("message"),Constants.message);
+    }
+
+    @Test(priority = 3)
+    public void correctBoth(){
+        String randomUsername = RandomStringUtils.randomAlphabetic(7);
+        Response response = steps.sendRequest(steps.jsonBody(randomUsername,Constants.correctPassword));
+        Assert.assertEquals(response.statusCode(),201);
+        Assert.assertNotNull(response.jsonPath().getString(Constants.userIdResponse));
+        Assert.assertEquals(response.jsonPath().getString("username"),randomUsername);
+    }
+}
